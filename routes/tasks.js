9bbb454b17ea1c4daec778a9475a2ad9b1aa374b
piotrek368 <63188949+piotrek368@users.noreply.github.com>
2020-04-10 14:59:27 +0200

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,7 +4,14 @@ const store = require('../store')
 router.prefix('/tasks')
 
 router.post('/add', async (ctx, next) => {
-await store.createTask(ctx.request.body.title)
-ctx.redirect('/')
+  await store.createTask(ctx.request.body.title)
+  ctx.redirect('/')
 })
-module.exports = router;
+
+router.post('/updateStatus', async (ctx, next) => {
+  const { id, status } = ctx.request.body
+  await store.updateTaskStatus(id, status)
+  ctx.status = 200
+})
+
+module.exports = router
